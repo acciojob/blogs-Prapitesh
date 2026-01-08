8e266eaac51e7c1b135b31723ceb98f177db0151
Prapitesh <aitipamulaprapitesh02@gmail.com>
2026-01-08 16:55:54 +0530

    done2

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,6 @@
 package com.driver.test;
 
-import com.driver.controller.BlogController;
-import com.driver.controller.ImageController;
-import com.driver.controller.UserController;
+import com.driver.controller.*;
 import com.driver.models.*;
 import com.driver.repositories.*;
 import com.driver.services.*;
@@ -20,7 +18,7 @@ import static org.mockito.Mockito.*;
 @RunWith(MockitoJUnitRunner.class)
 public class TestCases {
 
-    /* ===================== USER ===================== */
+    /* ================= USER ================= */
 
     @Mock
     private UserRepository userRepository;
@@ -36,10 +34,10 @@ public class TestCases {
 
         when(userRepository.save(any(User.class))).thenReturn(user);
 
-        User savedUser = userService.createUser("john", "1234");
+        User saved = userService.createUser("john", "1234");
 
-        assertNotNull(savedUser);
-        assertEquals("john", savedUser.getUsername());
+        assertNotNull(saved);
+        assertEquals("john", saved.getUsername());
     }
 
     @Test
@@ -49,7 +47,7 @@ public class TestCases {
         verify(userRepository, times(1)).deleteById(1);
     }
 
-    /* ===================== BLOG ===================== */
+    /* ================= BLOG ================= */
 
     @Mock
     private BlogRepository blogRepository;
@@ -63,25 +61,27 @@ public class TestCases {
         user.setId(1);
 
         Blog blog = new Blog();
-        blog.setTitle("My Blog");
-        blog.setContent("Content");
+        blog.setTitle("Test Blog");
 
         when(userRepository.findById(1)).thenReturn(Optional.of(user));
         when(blogRepository.save(any(Blog.class))).thenReturn(blog);
 
-        Blog savedBlog = blogService.createAndReturnBlog(1, "My Blog", "Content");
+        Blog saved = blogService.createAndReturnBlog(1, "Test Blog", "Content");
 
-        assertEquals("My Blog", savedBlog.getTitle());
+        assertEquals("Test Blog", saved.getTitle());
     }
 
     @Test
     public void testDeleteBlog() {
+        when(blogRepository.existsById(1)).thenReturn(true);
         doNothing().when(blogRepository).deleteById(1);
+
         blogService.deleteBlog(1);
-        verify(blogRepository, times(1)).deleteById(1);
+
+        verify(blogRepository).deleteById(1);
     }
 
-    /* ===================== IMAGE ===================== */
+    /* ================= IMAGE ================= */
 
     @Mock
     private ImageRepository imageRepository;
@@ -95,38 +95,37 @@ public class TestCases {
         blog.setId(1);
 
         Image image = new Image();
-        image.setImageUrl("img1");
-        image.setHeight(100);
+        image.setImageUrl("img");
         image.setWidth(100);
+        image.setHeight(100);
 
         when(blogRepository.findById(1)).thenReturn(Optional.of(blog));
         when(imageRepository.save(any(Image.class))).thenReturn(image);
 
-        Image savedImage = imageService.addImage(1, "img1", "100x100");
+        Image saved = imageService.addImage(1, "img", "100x100");
 
-        assertNotNull(savedImage);
-        assertEquals("img1", savedImage.getImageUrl());
+        assertEquals("img", saved.getImageUrl());
     }
 
     @Test
     public void testDeleteImage() {
         doNothing().when(imageRepository).deleteById(1);
         imageService.deleteImage(1);
-        verify(imageRepository, times(1)).deleteById(1);
+        verify(imageRepository).deleteById(1);
     }
 
     @Test
     public void testCountImagesInScreen() {
-        Image image1 = new Image();
-        image1.setWidth(200);
-        image1.setHeight(200);
-        Image image2 = new Image();
-        image2.setWidth(800);
-        image2.setHeight(800);
-        List<Image> images = Arrays.asList(image1, image2);
+        Image i1 = new Image();
+        i1.setWidth(200);
+        i1.setHeight(200);
+
+        Image i2 = new Image();
+        i2.setWidth(800);
+        i2.setHeight(800);
 
         Blog blog = new Blog();
-        blog.setImages(images);
+        blog.setImages(Arrays.asList(i1, i2));
 
         when(blogRepository.findById(1)).thenReturn(Optional.of(blog));
 
@@ -135,7 +134,7 @@ public class TestCases {
         assertEquals(1, count);
     }
 
-    /* ===================== CONTROLLERS ===================== */
+    /* ================= CONTROLLERS ================= */
 
     @InjectMocks
     private UserController userController;
@@ -148,39 +147,30 @@ public class TestCases {
 
     @Test
     public void testUserControllerCreate() {
-        User user = new User();
-        user.setUsername("alex");
+        when(userService.createUser(anyString(), anyString()))
+                .thenReturn(new User());
 
-        when(userService.createUser(anyString(), anyString())).thenReturn(user);
-
-        String  response = userController.createUser("alex", "123").getStatusCode().toString();
-
-        assertEquals("201", response);
+        assertEquals(201,
+                userController.createUser("a", "b").getStatusCodeValue());
     }
 
     @Test
     public void testBlogControllerCreate() {
-        Blog blog = new Blog();
-        blog.setTitle("Demo");
-
         when(blogService.createAndReturnBlog(anyInt(), anyString(), anyString()))
-                .thenReturn(blog);
+                .thenReturn(new Blog());
 
-        String response = blogController.createBlog(1, "Demo", "Content").getStatusCode().toString();
-
-        assertEquals("201", response);
+        assertEquals(201,
+                blogController.createBlog(1, "t", "c").getStatusCodeValue());
     }
 
     @Test
     public void testImageControllerAdd() {
-        Image image = new Image();
-        image.setImageUrl("img");
-
         when(imageService.addImage(anyInt(), anyString(), anyString()))
-                .thenReturn(image);
+                .thenReturn(new Image());
 
-       String response = imageController.addImage(1, "img", "100x100").getBody();
+        String body =
+                imageController.addImage(1, "img", "100x100").getBody();
 
-        assertEquals("img", response);
+        assertEquals("Added image successfully", body);
     }
 }
